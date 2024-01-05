9b2d042ae6d1015b0b09ad68f79cfa99878a31eb
architaggarwal786 <147228397+architaggarwal786@users.noreply.github.com>
2024-01-05 11:39:56 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+ <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
+
+    <script>
+      var typed = new Typed("#element", {
+        strings: ["Electrical Engineering", "Web Developer"],
+        typeSpeed: 50,
+      });
+    </script>
